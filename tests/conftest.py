f10e6802510e85d22ac5468d9268e2580f22ec37
pranav-geolabs <pranav@geometrylabs.io>
2021-02-08 18:58:21 -0800

    add tests

---
+++ tests/conftest.py
@@ -0,0 +1,18 @@
+import os
+from typing import Generator
+
+import pytest
+from fastapi.testclient import TestClient
+
+os.environ["ENV_FILE"] = os.path.join(
+    os.path.abspath(os.path.dirname(__file__)),
+    ".env.test",
+)
+
+from app.main import app
+
+
+@pytest.fixture(scope="module")
+def client() -> Generator:
+    with TestClient(app) as c:
+        yield c

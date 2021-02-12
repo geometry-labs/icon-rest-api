e967dee6767597522265e723ac59c35ab5849a45
rob <robcannonxyz@gmail.com>
2021-02-11 23:32:29 -0700

    adding new docker compose vars and tests setup

--- app/core/config.py
+++ app/core/config.py
@@ -1,4 +1,4 @@
-from os import environ, getenv
+import os
 
 from pydantic import AnyHttpUrl, BaseSettings
 
@@ -8,16 +8,17 @@ class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     PROJECT_NAME: str
 
-    MONGO_URL: str
-    MONGO_USER: str
-    MONGO_PASS: str
+    MONGO_HOST: str
+    MONGO_USERNAME: str
+    MONGO_PASSWORD: str
+
+    SERVER_HOST: str
 
     class Config:
         case_sensitive = True
 
 
-if getenv("ENV_FILE"):
-    settings = Settings(_env_file=environ["ENV_FILE"])
+if os.environ.get("ENV_FILE", False):
+    settings = Settings(_env_file=os.environ.get("ENV_FILE"))
 else:
     settings = Settings()
-    
--- tests/conftest.py
+++ tests/conftest.py
@@ -4,10 +4,12 @@ from typing import Generator
 import pytest
 from fastapi.testclient import TestClient
 
-os.environ["ENV_FILE"] = os.path.join(
-    os.path.abspath(os.path.dirname(__file__)),
-    ".env.test",
-)
+ENVIRONMENT = os.environ.get("ENVIRONMENT", "local")
+if ENVIRONMENT == "local":
+    # `.env.local` should be ignored in dockerignore so as to fail in container
+    os.environ["ENV_FILE"] = os.path.join(
+        os.path.abspath(os.path.dirname(__file__)), ".env.local"
+    )
 
 from main import app
 

4a8da7707144361aa76b0fb0fd42b5fef2d56aeb
rob <robcannonxyz@gmail.com>
2021-02-13 18:11:33 -0700

    adding tests that can run in ci with fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import json
 import os
 from typing import Generator
 
@@ -11,6 +12,7 @@ if ENVIRONMENT == "local":
         os.path.abspath(os.path.dirname(__file__)), ".env.local"
     )
 
+from app.db.session import MongoClient
 from main import app
 
 
@@ -18,3 +20,42 @@ from main import app
 def client() -> Generator:
     with TestClient(app) as c:
         yield c
+
+
+def insert_fixture(fixture):
+    client = MongoClient
+    with open(
+        os.path.join(
+            os.path.abspath(os.path.dirname(__file__)), "fixtures", fixture + ".json"
+        )
+    ) as f:
+        fixture_json = json.load(f)
+    db = client["icon"]
+    db.blocks.insert_many(fixture_json)
+
+
+@pytest.fixture()
+def prep_blocks():
+    insert_fixture("blocks")
+    yield
+    client = MongoClient
+    db = client["icon"]
+    db.blocks.remove({})
+
+
+@pytest.fixture()
+def prep_transactions():
+    insert_fixture("transactions")
+    yield
+    client = MongoClient
+    db = client["icon"]
+    db.blocks.remove({})
+
+
+@pytest.fixture()
+def prep_logs():
+    insert_fixture("logs")
+    yield
+    client = MongoClient
+    db = client["icon"]
+    db.blocks.remove({})
--- tests/routes/v1/test_blocks.py
+++ tests/routes/v1/test_blocks.py
@@ -1,28 +1,47 @@
-import requests
 from fastapi.testclient import TestClient
 
 from app.core.config import settings
 
 
-def test_get_block_latest(client: TestClient) -> None:
-    r = client.get(f"{settings.API_V1_STR}/blocks/")
+def test_get_block_latest(prep_blocks, client: TestClient) -> None:
+    r = client.get(f"{settings.ICON_REST_API_ENDPOINT_PREFIX}/blocks/")
     response = r.json()
     assert r.status_code == 200
     assert response
 
 
-def test_get_block_by_height() -> None:
-    url = settings.SERVER_HOST + "blocks/height/10000000"
-
-    response = requests.get(url)
-    assert response.status_code == 200
-
-
-def test_get_block_by_hash() -> None:
-    url = (
-        settings.SERVER_HOST
-        + "blocks/hash/ee0077a8bb433897ca7e00b52e0bdb461cf4c356ec817cd47a6570aa172b37dc"
+def test_get_block_by_height(prep_blocks, client: TestClient) -> None:
+    r = client.get(f"{settings.ICON_REST_API_ENDPOINT_PREFIX}/blocks/height/10000000")
+    response = r.json()
+    assert r.status_code == 200
+    assert (
+        response["hash"]
+        == "d3ca23378c6bdef5e17eb9dfb4fbcc7d17f5983570e8ddff6aff594d4dd44059"
     )
 
-    response = requests.get(url)
-    assert response.status_code == 200
+
+def test_get_block_by_height_error(prep_blocks, client: TestClient) -> None:
+    r = client.get(
+        f"{settings.ICON_REST_API_ENDPOINT_PREFIX}/blocks/height/1000000000000000"
+    )
+    response = r.json()
+    assert r.status_code == 404
+    assert response == {"detail": "Block not found"}
+
+
+def test_get_block_by_hash(prep_blocks, client: TestClient) -> None:
+    r = client.get(
+        f"{settings.ICON_REST_API_ENDPOINT_PREFIX}/blocks/hash/d3ca23378c6bdef5e17eb9dfb4fbcc7d17f5983570e8ddff6aff594d4dd44059"
+    )
+    response = r.json()
+    assert r.status_code == 200
+    assert response["number"] == 10000000
+
+
+def test_get_block_by_hash_error(prep_blocks, client: TestClient) -> None:
+    r = client.get(
+        f"{settings.ICON_REST_API_ENDPOINT_PREFIX}/blocks/hash/1111111111111111111111fbcc7d17f5983570e8ddff6aff594d4dd44059"
+    )
+    response = r.json()
+    assert r.status_code == 404
+    assert response == {"detail": "Block not found"}
